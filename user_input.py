6cecbfaf099be28b9cf72f1b8094b7e56547fa51
Ann <kairuthiann2@gmail.com>
2024-08-23 22:49:20 +0300

    Created user_input file and added python code with a personalized function asking for the users name, age and location

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Ask for user"s age
+age = input("How old are you? ")
+
+# Ask for the users location
+location = input("Where do you live? ")
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
